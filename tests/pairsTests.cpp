9eef09145ed9e07b7a1d6a0757e6b32569722f87
Daniel Wolf <dwolf@dannad.de>
2016-05-12 21:21:25 +0200

    Added getPairs function

---
+++ tests/pairsTests.cpp
@@ -0,0 +1,34 @@
+#include <gmock/gmock.h>
+#include "pairs.h"
+
+using namespace testing;
+using std::vector;
+using std::initializer_list;
+using std::pair;
+
+TEST(getPairs, emptyCollection) {
+	EXPECT_THAT(getPairs(vector<int>()), IsEmpty());
+}
+
+TEST(getPairs, oneElementCollection) {
+	EXPECT_THAT(getPairs(vector<int>{1}), IsEmpty());
+}
+
+TEST(getPairs, validCollection) {
+	{
+		auto actual = getPairs(vector<int>{ 1, 2 });
+		vector<pair<int, int>> expected{ {1, 2} };
+		EXPECT_THAT(actual, ElementsAreArray(expected));
+	}
+	{
+		auto actual = getPairs(vector<int>{ 1, 2, 3 });
+		vector<pair<int, int>> expected{ {1, 2}, {2, 3} };
+		EXPECT_THAT(actual, ElementsAreArray(expected));
+	}
+	{
+		auto actual = getPairs(vector<int>{ 1, 2, 3, 4 });
+		vector<pair<int, int>> expected{ {1, 2}, {2, 3}, {3, 4} };
+		EXPECT_THAT(actual, ElementsAreArray(expected));
+	}
+}
+
