542a5ee3d8f75c90745a70434b5bbbb6313e5942
Daniel Wolf <dwolf@dannad.de>
2016-06-14 17:38:41 +0200

    Added join function for strings

--- src/stringTools.h
+++ src/stringTools.h
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <boost/optional.hpp>
+#include <boost/lexical_cast.hpp>
 
 std::vector<std::string> splitIntoLines(const std::string& s);
 
@@ -16,3 +17,15 @@ boost::optional<char> toASCII(char32_t ch);
 std::string toASCII(const std::u32string& s);
 
 std::u32string utf8ToUtf32(const std::string& s);
+
+template<typename T>
+std::string join(T range, const std::string separator) {
+	std::string result;
+	bool isFirst = true;
+	for (const auto& element : range) {
+		if (!isFirst) result.append(separator);
+		isFirst = false;
+		result.append(boost::lexical_cast<std::string>(element));
+	}
+	return result;
+}
