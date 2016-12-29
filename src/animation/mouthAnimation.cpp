3bc4384b44bed5fce1a69eee970b41e55eaebb36
Daniel Wolf <dwolf@dannad.de>
2016-12-29 20:45:53 +0100

    Added overarching animation step that prevents long static segments
    See http://animateducated.blogspot.com/2016/10/lip-sync-animation-2.html?showComment=1478861729702#c2940729096183546458

--- src/animation/mouthAnimation.cpp
+++ src/animation/mouthAnimation.cpp
@@ -6,6 +6,7 @@
 #include "tweening.h"
 #include "timingOptimization.h"
 #include "targetShapeSet.h"
+#include "staticSegments.h"
 
 JoiningContinuousTimeline<Shape> animate(const BoundedTimeline<Phone> &phones, const ShapeSet& targetShapeSet) {
 	// Create timeline of shape rules
@@ -17,15 +18,19 @@ JoiningContinuousTimeline<Shape> animate(const BoundedTimeline<Phone> &phones, c
 	shapeRules = convertToTargetShapeSet(shapeRules, targetShapeSetPlusX);
 
 	// Animate in multiple steps
-	JoiningContinuousTimeline<Shape> animation = animateRough(shapeRules);
-	animation = optimizeTiming(animation);
-	animation = animatePauses(animation);
-	animation = insertTweens(animation);
-	animation = convertToTargetShapeSet(animation, targetShapeSet);
+	auto performMainAnimationSteps = [&targetShapeSet](const auto& shapeRules) {
+		JoiningContinuousTimeline<Shape> animation = animateRough(shapeRules);
+		animation = optimizeTiming(animation);
+		animation = animatePauses(animation);
+		animation = insertTweens(animation);
+		animation = convertToTargetShapeSet(animation, targetShapeSet);
+		return animation;
+	};
+	const JoiningContinuousTimeline<Shape> result = avoidStaticSegments(shapeRules, performMainAnimationSteps);
 
-	for (const auto& timedShape : animation) {
+	for (const auto& timedShape : result) {
 		logTimedEvent("shape", timedShape);
 	}
 
-	return animation;
+	return result;
 }
