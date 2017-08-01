e5c7215cc5583fe511e8591c326a4cca4d39fb72
Daniel Wolf <dwolf@dannad.de>
2017-08-01 18:19:07 +0200

    Added UTF8-CPP library

---
+++ lib/header-only.c
@@ -0,0 +1,7 @@
+/*
+
+This library is header-only.
+Due to limitations in CMake, I added this dummy code file.
+For details, see http://cmake.3232098.n2.nabble.com/Cannot-set-FOLDER-property-to-an-interface-header-only-target-td7592375.html
+
+*/
