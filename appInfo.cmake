e5c39efeebe87da3618f532248844e9e1968efc4
Daniel Wolf <dwolf@dannad.de>
2017-09-27 21:00:41 +0200

    Moved main executable into its own directory along with its build logic
    This will make it easier to add other artifacts that require build steps.

---
+++ appInfo.cmake
@@ -0,0 +1,8 @@
+cmake_minimum_required(VERSION 3.2)
+
+set(appName "Rhubarb Lip Sync")
+set(appVersionMajor 1)
+set(appVersionMinor 6)
+set(appVersionPatch 0)
+set(appVersionSuffix "")
+set(appVersion "${appVersionMajor}.${appVersionMinor}.${appVersionPatch}${appVersionSuffix}")
