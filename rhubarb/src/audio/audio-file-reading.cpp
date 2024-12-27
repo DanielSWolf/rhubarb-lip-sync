91390784e7a8ebaffec4cada4413bed1c9a247cb
Daniel Wolf <dwolf@dannad.de>
2024-12-27 11:15:38 +0100

    Enable C++23

--- rhubarb/src/audio/audio-file-reading.cpp
+++ rhubarb/src/audio/audio-file-reading.cpp
@@ -7,7 +7,6 @@
 #include "ogg-vorbis-file-reader.h"
 #include "wave-file-reader.h"
 
-using fmt::format;
 using std::runtime_error;
 using std::string;
 using std::filesystem::path;
@@ -21,13 +20,13 @@ std::unique_ptr<AudioClip> createAudioFileClip(path filePath) {
         if (extension == ".ogg") {
             return std::make_unique<OggVorbisFileReader>(filePath);
         }
-        throw runtime_error(format(
+        throw runtime_error(fmt::format(
             "Unsupported file extension '{}'. Supported extensions are '.wav' and '.ogg'.",
             extension
         ));
     } catch (...) {
         std::throw_with_nested(
-            runtime_error(format("Could not open sound file {}.", filePath.u8string()))
+            runtime_error(fmt::format("Could not open sound file {}.", filePath.u8string()))
         );
     }
 }
